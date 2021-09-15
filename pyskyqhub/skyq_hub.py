e53eea29db5bc6965145928b71f9d54eb18182a7
RogerSelwyn <rogers@selwyn.org.uk>
2021-09-15 10:12:16 +0100

    Code tidy up

--- pyskyqhub/skyq_hub.py
+++ pyskyqhub/skyq_hub.py
@@ -6,7 +6,8 @@ from dataclasses import dataclass, field
 
 import aiohttp
 
-from .const import CONNECTION_ERROR, DATA_ERROR, MAC_REGEX  # , test_response
+from .const import MAC_REGEX  # , test_response
+from .const import CONNECTION_ERROR, DATA_ERROR
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -37,7 +38,9 @@ class SkyQHub:
         """Retrieve data from Sky Hub and return parsed result."""
         parseddata = None
         try:
-            async with getattr(self.websession, "get")(self.url,) as response:
+            async with getattr(self.websession, "get")(
+                self.url,
+            ) as response:
                 if response.status == HTTP_OK:
                     if self._connection_failed:
                         self._log_message(
@@ -48,7 +51,7 @@ class SkyQHub:
                         )
                     responsedata = await response.text()
                     # responsedata = test_response
-                    parseddata = _parse_skyhub_response(responsedata)
+                    parseddata = await self._async_parse_skyhub_response(responsedata)
                     if self._dataparse_failed:
                         self._log_message(
                             "Response data from Sky Hub corrected",
@@ -78,55 +81,47 @@ class SkyQHub:
             else:
                 message = f"Invalid response from Sky Hub: {err}"
             self._log_message(
-                message, level=ERROR, error_type=DATA_ERROR,
+                message,
+                level=ERROR,
+                error_type=DATA_ERROR,
             )
             return
 
-    def _log_message(
-        self, log_message, unset_error=False, level=ERROR, error_type=None
-    ):
+    async def _async_parse_skyhub_response(self, data_str):
+        """Parse the Sky Hub data format."""
+        pattmatch = re.search("attach_dev = '(.*)'", data_str)
+        if pattmatch is None:
+            raise OSError("Error: Impossible to fetch data from Sky Hub. Try to reboot the router.")
+        patt = pattmatch.group(1)
+
+        dev = [patt1.split(",") for patt1 in patt.split("<lf>")]
+
+        devices = []
+        for i, dvc in enumerate(dev):
+            if not MAC_REGEX.match(dvc[1]):
+                raise RuntimeError(f"Error: MAC address {dvc[1]} not in correct format.")
+
+            mac = dvc[1]
+            name = dvc[0]
+            devices.append(_Device(mac, name))
+        return devices
+
+    def _log_message(self, log_message, unset_error=False, level=ERROR, error_type=None):
         if error_type == CONNECTION_ERROR:
             if self._connection_failed and not unset_error:
                 _LOGGER.debug(log_message)
                 return
-            if unset_error:
-                self._connection_failed = False
-            else:
-                self._connection_failed = True
+            self._connection_failed = not unset_error
         if error_type == DATA_ERROR:
             if self._dataparse_failed and not unset_error:
                 _LOGGER.debug(log_message)
                 return
-            if unset_error:
-                self._dataparse_failed = False
-            else:
-                self._dataparse_failed = True
+            self._dataparse_failed = not unset_error
         if level == ERROR:
             _LOGGER.error(log_message)
         if level == INFO:
             _LOGGER.info(log_message)
         return
-
-
-def _parse_skyhub_response(data_str):
-    """Parse the Sky Hub data format."""
-    pattmatch = re.search("attach_dev = '(.*)'", data_str)
-    if pattmatch is None:
-        raise OSError(
-            "Error: Impossible to fetch data from Sky Hub. Try to reboot the router."
-        )
-    patt = pattmatch.group(1)
-
-    dev = [patt1.split(",") for patt1 in patt.split("<lf>")]
-
-    devices = []
-    for dvc in dev:
-        if MAC_REGEX.match(dvc[1]):
-            devices.append(_Device(dvc[1], dvc[0]))
-        else:
-            raise RuntimeError(f"Error: MAC address {dvc[1]} not in correct format.")
-
-    return devices
 
 
 @dataclass
